b73dd07b6ba13f6a8e2fedd15815c354e9a0d3ec
tusharkumar <tusharkumar27864@gmail.com>
2023-06-28 16:49:39 +0530

    craeting an ec2 instance

--- ec2-module/main.tf
+++ ec2-module/main.tf
@@ -1,18 +1,41 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name                = var.key_name
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.web.id]
+  subnet_id              = aws_subnet.web.id
 
   tags = merge(local.tags, {
     "Name" = "autoscaleupinfra-engineering-dev-s3-module"
   })
 }
 
+resource "aws_security_group" "web" {
+  name        = "my-security-group"
+  description = "My security group"
+
+  vpc_id = aws_vpc.main.id
+
+  # Define your security group rules here
+  # ...
+}
+
+resource "aws_subnet" "web" {
+  cidr_block        = "10.0.0.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
 locals {
   tags = {
     "Created by"  = "Tushar"
     "Env"         = "dev"
-    "Company"     = "autostacelupinfra"
+    "Company"     = "autoscaleupinfra"
     "Automation"  = "terraform"
   }
 }
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ec2" {
   source = "./ec2-module"
 
   # Pass the values to the module
-  ami_id                       = "ami-0f9816f78187c68fb"
+  ami_id                       = "ami-01e442590cadbb4a6"
   instance_type                = "t2.micro"
   instance_names               = ["autoscaleupinfra-engineering-dev-s3-module"]
   key_name                     = "tushar-key"
